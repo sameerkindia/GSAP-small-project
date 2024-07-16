8ca88c6e1a0b99989ee58d6c37779daa764dbbfc
sameerExpansionjs <sameer@expansionjs.com>
2024-07-16 17:04:13 +0530

    to and from function

---
+++ script.js
@@ -0,0 +1,18 @@
+// gsap.to("#box-1", {
+//   x: "1000px",
+//   rotate: 360,
+//   backgroundColor: "white",
+//   transform: "translateY(500px)",
+//   borderRadius: "100px",
+//   duration: 1,
+//   delay: 2,
+// });
+gsap.from("#box-1", {
+  x: "1000px",
+  rotate: 360,
+  backgroundColor: "red",
+  transform: "translateY(500px)",
+  borderRadius: "100px",
+  duration: 1,
+  delay: 2,
+});
