64b419401d59741cf6ca4de76510509876700cd5
sameerExpansionjs <sameer@expansionjs.com>
2024-07-16 17:16:47 +0530

    gsap timeline

--- script.js
+++ script.js
@@ -7,12 +7,41 @@
 //   duration: 1,
 //   delay: 2,
 // });
-gsap.from("#box-1", {
-  x: "1000px",
+
+// gsap.from("#box-1", {
+//   x: "1000px",
+//   rotate: 360,
+//   backgroundColor: "red",
+//   transform: "translateY(500px)",
+//   borderRadius: "100px",
+//   duration: 1,
+//   delay: 2,
+// });
+
+const tl = gsap.timeline();
+
+tl.to("#box1", {
+  x: 1000,
   rotate: 360,
-  backgroundColor: "red",
-  transform: "translateY(500px)",
   borderRadius: "100px",
+  transform: "translateY(100px)",
   duration: 1,
-  delay: 2,
+  scale: 1.2,
+  delay: 1,
+});
+tl.to("#box2", {
+  x: 800,
+  rotate: 360,
+  borderRadius: "100px",
+  transform: "translateY(100px)",
+  scale: 1.2,
+  duration: 1,
+});
+tl.to("#box3", {
+  x: 1000,
+  rotate: 360,
+  borderRadius: "100px",
+  transform: "translateY(100px)",
+  scale: 1.2,
+  duration: 1,
 });
